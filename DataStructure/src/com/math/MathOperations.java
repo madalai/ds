c978be48741c22e41ae84f6cd8a24c3a9cb9611b
madalai <madalai@MADALAI-LAP.oradev.oraclecorp.com>
2018-08-09 23:16:28 +0530

    Added the gcd and lcm for array

--- DataStructure/src/com/math/MathOperations.java
+++ DataStructure/src/com/math/MathOperations.java
@@ -15,6 +15,28 @@ public class MathOperations {
 		return (a*b)/gcd;
 	}
 	
+	public static int gcd(int arr[]){
+		if(arr.length == 1){
+			return arr[0];
+		}
+		int tmp = gcd(arr[0],arr[1]);
+		for(int i = 2; i < arr.length ;i++){
+			tmp = gcd(tmp,arr[i]);
+		}
+		
+		return tmp;
+	}
+	
+	public static int lcm(int arr[]){
+		
+		int gcd = gcd(arr);
+		int mul = 1;
+		for(int i = 0 ; i<arr.length ;i++){
+			mul = mul*arr[i];
+		}
+		
+		return mul/gcd;
+	}
 	public static void main(String[] args) {
 		System.out.println(MathOperations.lcm(12,15));
 		System.out.println(MathOperations.gcd(9, 15));
