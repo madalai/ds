0b51349b10b6f020cc140725110cbf18fdeefad1
madalai <madalai@MADALAI-LAP.oradev.oraclecorp.com>
2018-08-10 08:00:21 +0530

    changed LCM 2

--- DataStructure/src/com/math/MathOperations.java
+++ DataStructure/src/com/math/MathOperations.java
@@ -29,14 +29,17 @@ public class MathOperations {
 	
 	public static int lcm(int arr[]){
 		
-		 int mul = 1;
-	        int gcd = gcd(arr);
-	        for(int i = 0 ; i <arr.length ; i++){
-	            mul = (mul * arr[i])/gcd;
-	        }
-	        
-	        
-	        return mul*gcd;
+ 	   if(arr.length == 0){
+           return 0;
+       }
+       if(arr.length == 1)return arr[0];
+       
+       int temp = lcm(arr[0],arr[1]);
+       
+       for(int i = 2 ;i <arr.length ;i++){
+           temp = lcm(arr[i],temp);
+       }
+       return temp;
 	}
 	public static void main(String[] args) {
 		System.out.println(MathOperations.lcm(12,15));
